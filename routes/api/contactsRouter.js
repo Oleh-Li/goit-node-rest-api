3fe32ccf5bc91f29005a6f7a2ce63247b36db2b5
Oleh <oleh@broadlanddigital.co.uk>
2024-02-16 09:09:13 +0200

    added contact db and run server

---
+++ routes/api/contactsRouter.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import ctrl from "../../controllers/contactsControllers.js"
+// import validateBody from "../helpers/validateBody.js";
+// import { createContactSchema, updateContactSchema } from "../models/contact.js";
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", ctrl.getAllContacts);
+
+// contactsRouter.get("/:id", ctrl.getOneContact);
+
+// contactsRouter.delete("/:id", ctrl.deleteContact);
+
+// contactsRouter.post("/", validateBody(createContactSchema), ctrl.createContact);
+
+// contactsRouter.put("/:id", validateBody(updateContactSchema), ctrl.updateContact);
+
+export default contactsRouter;
